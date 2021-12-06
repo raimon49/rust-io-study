02df5a3d1cbc18d789e8067b7db48ff5223c5bb8
raimon <raimon49@hotmail.com>
2021-12-06 20:21:15 +0900

    Add a code about implement 'grep'

--- src/main.rs
+++ src/main.rs
@@ -24,4 +24,19 @@ fn main() {
             }
         }
     }
+    {
+        use std::io;
+        use std::io::prelude::*;
+
+        fn grep(target: &str) -> io::Result<()> {
+            let stdin = io::stdin();
+            for line_result in stdin.lock().lines() {
+                let line = line_result?;
+                if line.contains(target) {
+                    println!("{}", line);
+                }
+            }
+            Ok(())
+        }
+    }
 }
