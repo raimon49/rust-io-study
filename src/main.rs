c5320aa8de18a3351c7f307046fb5b56805223db
raimon <raimon49@hotmail.com>
2021-12-13 19:19:07 +0900

    Add a code about using std::io::sink

--- src/main.rs
+++ src/main.rs
@@ -93,11 +93,17 @@ fn main() {
     }
     {
         use std::io::Read;
+        use std::io::Write;
         use std::io;
 
         let mut buf = String::new();
         let mut e = io::empty(); // 読み出しは常に成功するが何も返さないreader実装
         e.read_to_string(&mut buf).unwrap();
         assert!(buf.is_empty());
+
+        let mut i = io::sink(); // 書き出しは常に成功するが何もしないwriter実装
+        let write_buf = vec![1, 2, 3, 5, 8];
+        let bytes = i.write(&write_buf).unwrap();
+        assert_eq!(bytes, 5);
     }
 }
